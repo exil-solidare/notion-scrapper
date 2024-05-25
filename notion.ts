46defde17e8e2147348ca7d641f42849ad3fa33e
zakutnya  <yazakutny@gmail.com>
2024-05-25 12:47:24 +0300

    what else should i do

--- notion.ts
+++ notion.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const NOTION_DB = process.env.NOTION_DB;
+console.log(`Here we go: ${process.env.NOTION_TOKEN}`);
 
 export default new Client({
     auth: process.env.NOTION_TOKEN,
